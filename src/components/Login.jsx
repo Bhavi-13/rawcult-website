6dd13a95aa4a641ed2ccb3c46bb38db89c530d3b
Abhijeet Bhandari <abhijeetbhandari84@gmail.com>
2023-08-11 16:20:50 +0530

    Selling and Product Approval Form connected to BE

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import "../scss/Login.scss";
+import axios from "axios";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (req, res) => {
+    try {
+      const response = await axios.post(
+        "https://rawcult-be.onrender.com/auth/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+      // console.log("Login Successful!");
+      alert("User Logged In Sucessfully!");
+    } catch (error) {
+      console.error("Login Failed!", error);
+      alert(error.response.data.msg);
+    }
+  };
   return (
     <div className="login">
       <div className="content-container">
@@ -8,12 +30,26 @@ function Login() {
           <div className="simple-login">
             <h3>Login Form</h3>
             <label htmlFor="email">Email Address</label>
-            <input type="email" placeholder="Email Address" />
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Password" />
-            <span className="forgot"><a href="#">Forgot Password</a></span>
-            <button>Login</button>
-            <span>Don't Have an Account?<a href="#">Register Here</a></span>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span className="forgot">
+              <a href="#">Forgot Password</a>
+            </span>
+            <button onClick={handleLogin}>Login</button>
+            <span>
+              Don't Have an Account?<a href="#">Register Here</a>
+            </span>
           </div>
           <p>or</p>
           <button>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 function Register() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post(
+        "https://rawcult-be.onrender.com/auth/register",
+        {
+          name: fullName,
+          email,
+          password,
+        }
+      );
+
+      console.log("Registration successful!");
+      // You can add further logic here, such as redirecting the user or showing a success message.
+      alert(response.data.msg);
+    } catch (error) {
+      console.error("Registration error!");
+      // Handle error, show error message, etc.
+      alert(error.response.data.msg);
+    }
+  };
   return (
     <div className="login">
-    <div className="content-container">
-      <div className="content">
-        <div className="simple-login">
+      <div className="content-container">
+        <div className="content">
+          <div className="simple-login">
             <h3>Register Form</h3>
             <label htmlFor="name">Full Name</label>
-            <input type="text" name="name" id="name" placeholder='Full Name'/>
-          <label htmlFor="email">Email Address</label>
-          <input type="email" placeholder="Email Address" />
-          <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Password" />
-          <button>Register</button>
-          <span>Already Have an Account?<a href="#">Login Here</a></span>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleRegister}>Register</button>
+            <span>
+              Already Have an Account? <a href="#">Login Here</a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
